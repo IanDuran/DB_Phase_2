d1b754ab373e9d73c1981db2d20e7ebba1c894b7
JosueCuberoSanchez <josue.cubero@ucr.ac.cr>
2017-05-18 18:08:15 -0600

    Mitad

--- src/main/java/DataClases/ExerciseLog.java
+++ src/main/java/DataClases/ExerciseLog.java
@@ -1,7 +1,15 @@
 package DataClases;
 
+import java.util.Date;
+
 /**
  * Created by Ian on 13/5/2017.
  */
 public class ExerciseLog extends LogEntry {
+    Exercise exercise;
+
+    public ExerciseLog(Date date, ActionType type, int artificial_key, Administrator administrator,Exercise exercise) {
+        super(date, type, artificial_key, administrator);
+        this.exercise = exercise;
+    }
 }
