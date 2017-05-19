e5490afb12f445a29137f96241e3ffcee0980f5a
Ian Duran <ianduran60@gmail.com>
2017-05-18 18:51:34 -0600

    Answer and Procedure inheritances

--- src/main/java/DataClases/Procedure.java
+++ src/main/java/DataClases/Procedure.java
@@ -1,7 +1,18 @@
 package DataClases;
 
+import java.util.List;
+
 /**
  * Created by Ian on 13/5/2017.
  */
 public class Procedure {
+    private List<ProcedureLog> logs;
+
+    public List<ProcedureLog> getLogs() {
+        return logs;
+    }
+
+    public void setLogs(List<ProcedureLog> logs) {
+        this.logs = logs;
+    }
 }
---
+++ src/main/java/DataClases/TextProcedure.java
@@ -0,0 +1,16 @@
+package DataClases;
+
+/**
+ * Created by Ian on 5/18/2017.
+ */
+public class TextProcedure extends Procedure{
+    private String text;
+
+    public TextProcedure(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
